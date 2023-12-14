1a8d47b49897c82fa31495569d056f81a044d5ac
raphsenn <raphaelsenn@outlook.de>
2023-12-14 07:48:28 +0100

    gaussnewton

---
+++ gauss_newton.hpp
@@ -0,0 +1,9 @@
+#ifndef GAUSS_NEWTON_HPP
+#define GAUSS_NEWTON_HPP
+
+using FunctionType = std::function<std::vector<std::tuple<float, float>>(std::vector<std::tuple<float, float>>, const std::vector<float>&)>;
+using GradientFunctionType = std::function<std::vector<std::tuple<float, float>>(std::vector<std::tuple<float, float>>, const std::vector<float>&)>;
+
+std::vector<float> gaussNewton(FunctionType func, GradientFunctionType gradient, std::vector<std::tuple<float, float>> x, const std::vector<float>& initialCoefficients, int max_iterations = 100, float tolerance = 1e-6);
+
+#endif // GAUSS_NEWTON_HPP
